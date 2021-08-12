69d4210ee17962d5ee7c0ee2ebe7730494bab582
RadinalSaragih <radinalsaragih13@protonmail.com>
2021-08-12 13:35:38 +0700

    Updates, and added the xresources plugin.

--- config.def.h
+++ config.def.h
@@ -1,22 +1,43 @@
 /* Default settings (can be overriden by command line.) */
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static const char *prompt  = NULL; /* -p  option; prompt to the left of input field */
-static const char *fonts[] = {"Hack Regular:size=11"};  /* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {"monospacer:size=11"};  /* -fn option overrides fonts[0]; default X11 font or font set */
 
 /* colors */
-static const char col_grey[] = "#262626";
-static const char col_black[] = "#181818";
-static const char col_white[] = "#949494";
-static const char col_orange[] = "#eb7d00";
-static const char *colors[SchemeLast][2] = {
-  /*                fg         bg       */
-  [SchemeNorm] = { col_white, col_grey },
-  [SchemeSel]  = { col_black, col_orange },
-  [SchemeOut]  = { col_black, "#00ffff" },
-  [SchemeHp]   = { col_orange, "#333333" }
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char selbgcolor[]  = "#005577";
+static char selbgcolor[]  = "#005577";
+static char hpfgcolor[]   = "#ffffff";
+statuc char hpbgcolor[]   = "#000000";
+
+static char *colors[SchemeLast][2] = {
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
+	[SchemeHp]   = { hpfgcolor, hpbgcolor }
 };
 
-static unsigned int lines          = 0; /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines          = 0; /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lineheight     = 0; /* -h option; minimum height of a menu line */
 static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " "; /* Characters not considered part of a word while deleting words, for example: " /?\"&[]" */
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+
+	{ "hpfgcolor",  STRING, &hpfgcolor }, 
+	{ "hpbgcolor",  STRING, &hpbgcolor }, 
+
+	{ "prompt",      STRING, &prompt },
+	{ "topbar",      INTEGER, &topbar },
+};
--- config.h
+++ config.h
@@ -1,22 +1,45 @@
 /* Default settings (can be overriden by command line.) */
-static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
-static const char *prompt  = NULL; /* -p  option; prompt to the left of input field */
-static const char *fonts[] = {"Hack Regular:size=11"};  /* -fn option overrides fonts[0]; default X11 font or font set */
-
-/* colors */
-static const char col_grey[] = "#262626";
-static const char col_black[] = "#181818";
-static const char col_white[] = "#ebdbb2";
-static const char col_green[] = "#4b6e50";
-static const char *colors[SchemeLast][2] = {
-	/*                fg         bg       */
-	[SchemeNorm] = { col_white, col_grey },
-	[SchemeSel]  = { col_white, col_green },
-	[SchemeOut]  = { col_black, col_green },
-	[SchemeHp]   = { col_green, col_grey }
+static int topbar    = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static char *prompt  = NULL; /* -p  option; prompt to the left of input field */
+static char font[] = "monospace:size=10"; /* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = { 
+  font,
+  "monospace:size=10" 
 };
 
-static unsigned int lines          = 0; /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* colors */
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char hpfgcolor[]   = "#ffffff";
+static char hpbgcolor[]   = "#000000";
+
+static char *colors[SchemeLast][2] = {
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
+	[SchemeHp]   = { hpfgcolor, hpbgcolor }
+};
+
+static unsigned int lines          = 0; /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lineheight     = 0; /* -h option; minimum height of a menu line */
 static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " "; /* Characters not considered part of a word while deleting words, for example: " /?\"&[]" */
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+
+	{ "hpfgcolor",  STRING, &hpfgcolor }, 
+	{ "hpbgcolor",  STRING, &hpbgcolor }, 
+
+	{ "prompt",      STRING, &prompt },
+	{ "topbar",      INTEGER, &topbar },
+};
