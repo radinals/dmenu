0b7d580110411836df6e23768101bd1eda99a291
radinals <radinalsaragih@gmail.com>
2024-11-19 22:35:27 +0700

    added centered menu patch

--- config.h
+++ config.h
@@ -7,6 +7,13 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"monospace:size=10"
 };
+
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_width_ratio = 2.0f;
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
+static int border_width = 0;
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
--- dmenu.c
+++ dmenu.c
@@ -25,7 +25,7 @@
 #define INTERSECT(x, y, w, h, r)                                               \
 	(MAX(0, MIN((x) + (w), (r).x_org + (r).width) - MAX((x), (r).x_org)) * \
 	 MAX(0, MIN((y) + (h), (r).y_org + (r).height) - MAX((y), (r).y_org)))
-#define TEXTW(X)  (drw_fontset_getwidth(drw, (X)) + lrpad)
+#define TEXTW(X) (drw_fontset_getwidth(drw, (X)) + lrpad)
 
 /* enums */
 enum {
@@ -106,6 +106,15 @@ calcoffsets(void)
 		if ((i += (lines > 0) ? bh : textw_clamp(prev->left->text, n)) >
 		    n)
 			break;
+}
+
+static int
+max_textw(void)
+{
+	int len = 0;
+	for (struct item *item = items; item && item->text; item++)
+		len = MAX(TEXTW(item->text), len);
+	return len;
 }
 
 static void
@@ -709,6 +718,7 @@ setup(void)
 	bh = drw->fonts->h + 2;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
+	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 #ifdef XINERAMA
 	i = 0;
 	if (parentwin == root && (info = XineramaQueryScreens(dpy, &n))) {
@@ -743,9 +753,25 @@ setup(void)
 				if (INTERSECT(x, y, 1, 1, info[i]) != 0)
 					break;
 
-		x = info[i].x_org;
-		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
-		mw = info[i].width;
+		/*
+		        x = info[i].x_org;
+		        y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
+		        mw = info[i].width;
+		*/
+
+		if (centered) {
+			mw = MIN(MAX(max_textw() + promptw,
+			             info[i].width / menu_width_ratio),
+			         info[i].width);
+			x = info[i].x_org + ((info[i].width - mw) / 2);
+			y = info[i].y_org +
+			    ((info[i].height - mh) / menu_height_ratio);
+		} else {
+			x = info[i].x_org;
+			y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
+			mw = info[i].width;
+		}
+
 		XFree(info);
 	} else
 #endif
@@ -753,9 +779,23 @@ setup(void)
 		if (!XGetWindowAttributes(dpy, parentwin, &wa))
 			die("could not get embedding window attributes: 0x%lx",
 			    parentwin);
-		x = 0;
-		y = topbar ? 0 : wa.height - mh;
-		mw = wa.width;
+		/*
+		        x = 0;
+		        y = topbar ? 0 : wa.height - mh;
+		        mw = wa.width;
+		*/
+
+		if (centered) {
+			mw = MIN(MAX(max_textw() + promptw,
+			             wa.width / menu_width_ratio),
+			         wa.width);
+			x = (wa.width - mw) / 2;
+			y = (wa.height - mh) / 2;
+		} else {
+			x = 0;
+			y = topbar ? 0 : wa.height - mh;
+			mw = wa.width;
+		}
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = mw / 3; /* input width: ~33% of monitor width */
@@ -766,9 +806,12 @@ setup(void)
 	swa.background_pixel = scheme[SchemeNorm][ColBg].pixel;
 	swa.event_mask = ExposureMask | KeyPressMask | VisibilityChangeMask;
 	win =
-	    XCreateWindow(dpy, root, x, y, mw, mh, 0, CopyFromParent,
+	    XCreateWindow(dpy, root, x, y, mw, mh, border_width, CopyFromParent,
 	                  CopyFromParent, CopyFromParent,
 	                  CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
+
+	if (border_width)
+		XSetWindowBorder(dpy, win, scheme[SchemeNorm][ColFg].pixel);
 	XSetClassHint(dpy, win, &ch);
 
 	/* input methods */
@@ -897,8 +940,13 @@ main(int argc, char *argv[])
 			colortemp[3] = argv[++i];
 		else if (!strcmp(argv[i], "-w")) /* embedding window id */
 			embed = argv[++i];
-		else
+		else if (!strcmp(argv[i], "-bw"))
+			border_width = atoi(argv[++i]); /* border width */
+		else if (!strcmp(argv[i], "-c")) /* centers dmenu on screen */
+			centered = 1;
+		else {
 			usage();
+		}
 
 	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
 		fputs("warning: no locale support\n", stderr);
